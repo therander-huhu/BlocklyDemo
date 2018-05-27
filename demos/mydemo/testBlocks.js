0d82126081b78b664f68df025e35c4f1a0a9e39f
李广盟 <liguangmeng@liguangengdeMBP.lan>
2018-05-27 15:09:05 +0800

    随机数

---
+++ demos/mydemo/testBlocks.js
@@ -0,0 +1,12 @@
+Blockly.defineBlocksWithJsonArray([  // BEGIN JSON EXTRACT
+    // Block for colour picker.
+    {
+        "type": "start_tip",
+        "message0": "PressButton",
+        "nextStatement": null,
+        "colour": 60,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    
+    ]);
---
+++ demos/mydemo/testPython.js
@@ -0,0 +1,11 @@
+
+Blockly.Python['start_tip'] = function(block) {
+    let code = "ll";
+    return [code, Blockly.Python.ORDER_FUNCTION_CALL];
+};
+
+//生成随机数范围
+Blockly.Python['random_int'] = function(block) {
+    let code = "";
+    return [code, Blockly.Python.ORDER_FUNCTION_CALL];
+};
