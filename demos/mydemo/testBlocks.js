e40524795fcd20eb9591fccabda4054af996978f
ListenLeeLee <734246251@qq.com>
2018-05-31 00:35:37 +0800

    比较 且或

--- demos/mydemo/testBlocks.js
+++ demos/mydemo/testBlocks.js
@@ -8,5 +8,194 @@ Blockly.defineBlocksWithJsonArray([  // BEGIN JSON EXTRACT
         "tooltip": "",
         "helpUrl": ""
     },
-    
+    {
+        "type": "math_arithmetic_add",
+        "message0": "%1 + %2",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "A",
+                "check": "Number"
+            },
+            {
+                "type": "input_value",
+                "name": "B",
+                "check": "Number"
+            }
+        ],
+        "output": "Number",
+        "inputsInline": true,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "math_arithmetic_minus",
+        "message0": "%1 - %2",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "A",
+                "check": "Number"
+            },
+            {
+                "type": "input_value",
+                "name": "B",
+                "check": "Number"
+            }
+        ],
+        "output": "Number",
+        "inputsInline": true,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "math_arithmetic_multiply",
+        "message0": "%1 × %2",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "A",
+                "check": "Number"
+            },
+            {
+                "type": "input_value",
+                "name": "B",
+                "check": "Number"
+            }
+        ],
+        "output": "Number",
+        "inputsInline": true,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "math_arithmetic_divide",
+        "message0": "%1 ÷ %2",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "A",
+                "check": "Number"
+            },
+            {
+                "type": "input_value",
+                "name": "B",
+                "check": "Number"
+            }
+        ],
+        "output": "Number",
+        "inputsInline": true,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "logic_compare_gt",
+        "message0": "%1 > %2",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "A",
+                "check": "Number"
+            },
+            {
+                "type": "input_value",
+                "name": "B",
+                "check": "Number"
+            }
+        ],
+        "output": "Boolean",
+        "inputsInline": true,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "logic_compare_lt",
+        "message0": "%1 < %2",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "A",
+                "check": "Number"
+            },
+            {
+                "type": "input_value",
+                "name": "B",
+                "check": "Number"
+            }
+        ],
+        "output": "Boolean",
+        "inputsInline": true,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+
+    {
+        "type": "logic_compare_eq",
+        "message0": "%1 = %2",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "A",
+                "check": "Number"
+            },
+            {
+                "type": "input_value",
+                "name": "B",
+                "check": "Number"
+            }
+        ],
+        "output": "Boolean",
+        "inputsInline": true,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "logic_operation_and",
+        "message0": "%1 且 %2",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "A",
+                "check": "Boolean"
+            },
+            {
+                "type": "input_value",
+                "name": "B",
+                "check": "Boolean"
+            }
+        ],
+        "output": "Boolean",
+        "inputsInline": true,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "logic_operation_or",
+        "message0": "%1 或 %2",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "A",
+                "check": "Boolean"
+            },
+            {
+                "type": "input_value",
+                "name": "B",
+                "check": "Boolean"
+            }
+        ],
+        "output": "Boolean",
+        "inputsInline": true,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    }
     ]);
--- demos/mydemo/testPython.js
+++ demos/mydemo/testPython.js
@@ -4,8 +4,70 @@ Blockly.Python['start_tip'] = function(block) {
     return [code, Blockly.Python.ORDER_FUNCTION_CALL];
 };
 
-//生成随机数范围
-Blockly.Python['random_int'] = function(block) {
-    let code = "";
-    return [code, Blockly.Python.ORDER_FUNCTION_CALL];
+Blockly.Python['math_arithmetic_add'] = function(block) {
+    var argument0 = Blockly.Python.valueToCode(block, 'A',  Blockly.Python.ORDER_ADDITIVE);
+    var argument1 = Blockly.Python.valueToCode(block, 'B', Blockly.Python.ORDER_ADDITIVE);
+    var code = argument0 + "+" +argument1;
+    return [code, Blockly.Python.ORDER_ADDITIVE];
+};
+
+Blockly.Python['math_arithmetic_minus'] = function(block) {
+    var argument0 = Blockly.Python.valueToCode(block, 'A',  Blockly.Python.ORDER_ADDITIVE);
+    var argument1 = Blockly.Python.valueToCode(block, 'B', Blockly.Python.ORDER_ADDITIVE);
+    var code = argument0 + "-" +argument1;
+    return [code, Blockly.Python.ORDER_ADDITIVE];
+};
+
+Blockly.Python['math_arithmetic_multiply'] = function(block) {
+    var argument0 = Blockly.Python.valueToCode(block, 'A',  Blockly.Python.ORDER_MULTIPLICATIVE);
+    var argument1 = Blockly.Python.valueToCode(block, 'B', Blockly.Python.ORDER_MULTIPLICATIVE);
+    var code = argument0 + "*" +argument1;
+    return [code, Blockly.Python.ORDER_MULTIPLICATIVE];
+};
+
+Blockly.Python['math_arithmetic_divide'] = function(block) {
+    var argument0 = Blockly.Python.valueToCode(block, 'A',  Blockly.Python.ORDER_MULTIPLICATIVE);
+    var argument1 = Blockly.Python.valueToCode(block, 'B', Blockly.Python.ORDER_MULTIPLICATIVE);
+    var code = argument0 + "/" +argument1;
+    return [code, Blockly.Python.ORDER_MULTIPLICATIVE];
+};
+
+Blockly.Python['logic_compare_gt'] = function(block) {
+    var order = Blockly.Python.ORDER_RELATIONAL;
+    var argument0 = Blockly.Python.valueToCode(block, 'A', order);
+    var argument1 = Blockly.Python.valueToCode(block, 'B', order);
+    var code = argument0 + ">" +argument1;
+    return [code, order];
+};
+
+Blockly.Python['logic_compare_lt'] = function(block) {
+    var order = Blockly.Python.ORDER_RELATIONAL;
+    var argument0 = Blockly.Python.valueToCode(block, 'A', order);
+    var argument1 = Blockly.Python.valueToCode(block, 'B', order);
+    var code = argument0 + "<" +argument1;
+    return [code, order];
+};
+
+Blockly.Python['logic_compare_eq'] = function(block) {
+    var order = Blockly.Python.ORDER_RELATIONAL;
+    var argument0 = Blockly.Python.valueToCode(block, 'A', order);
+    var argument1 = Blockly.Python.valueToCode(block, 'B', order);
+    var code = argument0 + "==" +argument1;
+    return [code, order];
+};
+
+Blockly.Python['logic_operation_and'] = function(block) {
+    var order = Blockly.Python.ORDER_LOGICAL_AND;
+    var argument0 = Blockly.Python.valueToCode(block, 'A', order);
+    var argument1 = Blockly.Python.valueToCode(block, 'B', order);
+    var code = argument0 + "and" +argument1;
+    return [code, order];
+};
+
+Blockly.Python['logic_operation_or'] = function(block) {
+    var order = Blockly.Python.ORDER_LOGICAL_OR;
+    var argument0 = Blockly.Python.valueToCode(block, 'A', order);
+    var argument1 = Blockly.Python.valueToCode(block, 'B', order);
+    var code = argument0 + "or" +argument1;
+    return [code, order];
 };
